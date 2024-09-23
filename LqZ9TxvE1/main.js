0b2e35bcf1edabf7984310af362ebfff4104a2f0
Hanjie Liu <hanjieliu@Hanjies-MBP.attlocal.net>
2024-09-23 16:07:03 -0400

    shrink sizes

--- LqZ9TxvE1/main.js
+++ LqZ9TxvE1/main.js
@@ -243,13 +243,13 @@ $(function() {
        var grp = $("#others").children();
        var cnt = grp.length;
 
-       var temp,x;
-       for (var i = 0; i < cnt; i++) {
-           temp = grp[i];
-         x = Math.floor(Math.random() * cnt);
-         grp[i] = grp[x];
-         grp[x] = temp;
-     }
+       // var temp,x;
+       // for (var i = 0; i < cnt; i++) {
+       //  temp = grp[i];
+       //   x = Math.floor(Math.random() * cnt);
+       //   grp[i] = grp[x];
+       //   grp[x] = temp;
+       // }
      $(grp).remove();
      $("#others").append($(grp));
     }
